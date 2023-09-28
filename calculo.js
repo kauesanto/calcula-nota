4a541e98e802a081b9c45d7f296b5a5f68d7d88c
kauesanto <129408833+kauesanto@users.noreply.github.com>
2023-09-28 12:06:18 -0300

    nota

---
+++ calculo.js
@@ -0,0 +1,22 @@
+//Calculadora de idade 
+
+/*console.log("esta funcionando!");  
+
+let anoDeNascimento = window.prompt("Em que ano você nasceu?");
+console.log(anoDeNascimento);
+alert("Quem nasceu em" + anoDeNascimento + "vai fazer 17 anos em 2023 */ 
+
+//-------------------------------------------------------------------------//
+//Calculadora de nota 
+let nota1 = window.prompt("Nota do primeiro trimestre:");
+console.log(nota1);60
+let nota2 = window.prompt("Nota do segundo trimestre:");
+console.log(nota2);57
+let nota3 = window.prompt("Nota do terceiro trimestre:");
+console.log(nota3);70
+console.log(nota1+nota2+nota3);
+alert("Nota dos trimestres" = nota1 + nota2 +nota3);
+let resultado = nota1 + nota2 + nota3;
+console.log(resultado);
+let resultadoFinal = document.querySelector(".resultado final")
+alert("Nota dos  trimestres"  = result + nota1 + nota2 + nota3);
