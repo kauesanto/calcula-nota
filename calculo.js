8f212275d1f7e27530a9fb1734133a87a22dae68
kauesanto <129408833+kauesanto@users.noreply.github.com>
2023-09-29 10:31:18 -0300

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -1,22 +1,20 @@
 //Calculadora de idade 
-
-/*console.log("esta funcionando!");  
-
-let anoDeNascimento = window.prompt("Em que ano você nasceu?");
-console.log(anoDeNascimento);
-alert("Quem nasceu em" + anoDeNascimento + "vai fazer 17 anos em 2023 */ 
-
-//-------------------------------------------------------------------------//
+function idade(){
+  let anoDeNascimento = parseInt(prompt("Em que ano você nasceu?")
+  let idade = 2023 - anoDeNascimento;
+  alert("Quem nasceu em " + anoDeNascimento + "vai fazer" + idade  17 anos em 2023 */"
+}
 //Calculadora de nota 
-let nota1 = window.prompt("Nota do primeiro trimestre:");
-console.log(nota1);60
-let nota2 = window.prompt("Nota do segundo trimestre:");
-console.log(nota2);57
-let nota3 = window.prompt("Nota do terceiro trimestre:");
-console.log(nota3);70
-console.log(nota1+nota2+nota3);
-alert("Nota dos trimestres" = nota1 + nota2 +nota3);
+function notas(){
+
+let nota1 = parseFloat(prompt("Digite a nota do primeiro trimestre:")
+let nota2 = parseFloat(prompt("Digite a nota do segundo trimestre:")
+let nota3 = parseFloat(prompt("Digite nota do terceiro trimestre:")
+
 let resultado = nota1 + nota2 + nota3;
-console.log(resultado);
+
+if (resultado>=180){
+    alert("aprovado")
+}else
 let resultadoFinal = document.querySelector(".resultado final")
 alert("Nota dos  trimestres"  = result + nota1 + nota2 + nota3);
